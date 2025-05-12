04089c3e19d2b60787c59b47fc41d902a06711f8
JoshFlenniken <146684341+JoshFlenniken@users.noreply.github.com>
2025-05-12 16:04:26 -0400

    First upload

---
+++ app.py
@@ -0,0 +1,137 @@
+# ########################################
+# ########## SETUP
+
+from flask import Flask, render_template, request, redirect
+import database.db_connector as db
+
+PORT = 10984
+
+app = Flask(__name__)
+
+# ########################################
+# ########## ROUTE HANDLERS
+
+# READ ROUTES
+@app.route("/", methods=["GET"])
+def home():
+    try:
+        return render_template("home.j2")
+
+    except Exception as e:
+        print(f"Error rendering page: {e}")
+        return "An error occurred while rendering the page.", 500
+
+
+@app.route("/customers", methods=["GET"])
+def customers():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        # In query1, we use a JOIN clause to display the names of the homeworlds,
+        #       instead of just ID values
+        query1 = "SELECT * FROM Customers;"
+        customers = db.query(dbConnection, query1).fetchall()
+
+        # Render the bsg-people.j2 file, and also send the renderer
+        # a couple objects that contains bsg_people and bsg_homeworld information
+        return render_template(
+            "customers.j2", customers=customers
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+@app.route("/employees", methods=["GET"])
+def employees():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        # In query1, we use a JOIN clause to display the names of the homeworlds,
+        #       instead of just ID values
+        query1 = "SELECT * FROM Employees;"
+        employees = db.query(dbConnection, query1).fetchall()
+
+        # Render the bsg-people.j2 file, and also send the renderer
+        # a couple objects that contains bsg_people and bsg_homeworld information
+        return render_template(
+            "employees.j2", employees=employees
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+@app.route("/orders", methods=["GET"])
+def orders():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        # In query1, we use a JOIN clause to display the names of the homeworlds,
+        #       instead of just ID values
+        query1 = "SELECT * FROM Orders;"
+        orders = db.query(dbConnection, query1).fetchall()
+
+        # Render the bsg-people.j2 file, and also send the renderer
+        # a couple objects that contains bsg_people and bsg_homeworld information
+        return render_template(
+            "orders.j2", orders=orders
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+@app.route("/products", methods=["GET"])
+def products():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        # In query1, we use a JOIN clause to display the names of the homeworlds,
+        #       instead of just ID values
+        query1 = "SELECT * FROM Products;"
+        products = db.query(dbConnection, query1).fetchall()
+
+        # Render the bsg-people.j2 file, and also send the renderer
+        # a couple objects that contains bsg_people and bsg_homeworld information
+        return render_template(
+            "products.j2", products=products
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+
+
+# ########################################
+# ########## LISTENER
+
+if __name__ == "__main__":
+    app.run(
+        port=PORT, debug=True
+    )  # debug is an optional parameter. Behaves like nodemon in Node.
